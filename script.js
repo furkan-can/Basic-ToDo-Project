4ced17f052474b9a75824d17809460bc065cbe77
Furkan <furkancantavukcu98@gmail.com>
2022-07-11 13:45:30 +0300

    Added deleteTask and deleteAllTasks

--- script.js
+++ script.js
@@ -11,24 +11,24 @@ function eventListeners() {
     form.addEventListener('submit', addTask);
     taskList.addEventListener('click', deleteTask);
     btnAddNewTask.addEventListener('click', addTask);
-    brnDeleteAll.addEventListener('click', deleteAll);
+    brnDeleteAll.addEventListener('click', deleteAllTasks);
 }
 
 function addTask(e) {
     let control = true;
 
-    document.getElementById('task-list').childNodes.forEach(function(item){
-        if(item.innerText==input.value){
+    document.getElementById('task-list').childNodes.forEach(function (item) {
+        if (item.innerText == input.value) {
             control = false;
         }
     });
 
     if (input.value === '') {
         alert('Please add a task');
-    }else if(!control){
+    } else if (!control) {
         alert('Task already exists');
         input.value = '';
-    }else {
+    } else {
         const li = document.createElement('li');
         li.className = 'list-group-item list-group-item-secondary';
         li.appendChild(document.createTextNode(input.value));
@@ -44,4 +44,23 @@ function addTask(e) {
         input.value = '';
     }
     e.preventDefault();
+}
+
+function deleteTask(e) {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+    e.preventDefault();
+}
+
+function deleteAllTasks(e) {
+    if (confirm('Are you sure?')) {
+        taskList.childNodes.forEach(function (item) {
+            if (item.nodeType === 1) {
+                item.remove();
+            }
+        });
+    }
 }
